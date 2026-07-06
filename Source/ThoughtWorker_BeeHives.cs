3b8be01b8bfdf8efa27ffd1baf17854dd4ecae80
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2026-07-06 19:35:41 +0300

    traits

---
+++ Source/ThoughtWorker_BeeHives.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BiomesBees
+{
+	public class ThoughtWorker_BeeHives : ThoughtWorker
+	{
+
+		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			if (HiveUtility.disabled || ThoughtUtility.ThoughtNullified(p, def))
+			{
+				return ThoughtState.Inactive;
+			}
+			return HiveUtility.Hives.Any(hive => hive.Map == p.MapHeld);
+		}
+	}
+
+}
